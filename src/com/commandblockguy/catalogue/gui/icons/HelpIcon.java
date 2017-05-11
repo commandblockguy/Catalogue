80c148ce0b1b3871f2cf47464969b5b0bb6994a4
commandblockguy <ecesarz98@gmail.com>
2017-05-10 22:10:15 -0500

    Um, what?
    GitHub did not realize this was modified, apparently

--- src/com/commandblockguy/catalogue/gui/icons/HelpIcon.java
+++ src/com/commandblockguy/catalogue/gui/icons/HelpIcon.java
@@ -29,6 +29,7 @@ public class HelpIcon extends Icon {
 		player.sendMessage("Usage: ");
 		player.sendMessage(Catalogue.getPlugin().getCommand("catalogue").getUsage());
 		player.sendMessage(Catalogue.getPlugin().localization.getString("help.underscores"));
+		player.sendMessage(Catalogue.getPlugin().localization.getString("help.compasses"));
 		player.sendMessage("Filter Types: Amount, Buy Price, Item, Player, Sell Price, Town");
 		player.sendMessage("Sort Types: Amount, Buy Price, Item Name, Per Item Buy Price, Per Item Sell Price, Sell Price, Town Name");
 		player.sendMessage(Catalogue.getPlugin().localization.getString("help.desusage"));
